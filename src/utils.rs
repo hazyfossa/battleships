9323825beeaeb5223d92a4e19cb1bb620d7dc94c
hazyfossa <hazyfossa@gmail.com>
2025-09-28 00:56:46 +0500

    Simpler mimetype handling

--- src/utils.rs
+++ src/utils.rs
@@ -48,10 +48,11 @@ pub mod assets {
             let path = self.0.into();
 
             match Assets::get(path.as_str()) {
-                Some(content) => {
-                    let mime = mime_guess::from_path(path).first_or_octet_stream();
-                    ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
-                }
+                Some(content) => (
+                    [(header::CONTENT_TYPE, content.metadata.mimetype())],
+                    content.data,
+                )
+                    .into_response(),
                 None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
             }
         }
